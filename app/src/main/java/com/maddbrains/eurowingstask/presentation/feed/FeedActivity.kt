284b14ec9196638b2ca229ebc7341a02c6a3381a
Aneeb.Ahmed <aneeb.ahmed@veripark.com>
2020-10-22 15:31:58 +0500

    Add Project Structure

---
+++ app/src/main/java/com/maddbrains/eurowingstask/presentation/feed/FeedActivity.kt
@@ -0,0 +1,12 @@
+package com.maddbrains.eurowingstask.presentation.feed
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.maddbrains.eurowingstask.R
+
+class FeedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
